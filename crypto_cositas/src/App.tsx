69efa534fba019b8d5dc6a16804da95d3833d3a2
Juan Reyes <juanrreyesrincon@gmail.com>
2024-09-28 11:53:37 -0500

    Creación de logos
    Creación de logos

--- crypto_cositas/src/App.tsx
+++ crypto_cositas/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import anyaLogo from './assets/anya.svg'
 import './App.css'
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://anya.dev" target="_blank">
+          <img src={anyaLogo} className="logo anya" alt="Anya logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
